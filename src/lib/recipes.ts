3ef07d0a5a6b921d5053c68ecc3193786887b15a
Austin Simas <austin@simas.io>
2025-07-14 19:52:18 -0700

    Fix build: remove unused remark and html imports from recipes.ts

--- src/lib/recipes.ts
+++ src/lib/recipes.ts
@@ -1,8 +1,6 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
-import { remark } from 'remark';
-import html from 'remark-html';
 import { Recipe, RecipePreview, TagCount } from './types';
 
 const recipesDirectory = path.join(process.cwd(), 'src/content/recipes');
